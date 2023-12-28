e51ab8e1c796153b7cee5d514bac77f5bacdc3d2
Aayman-star <aayman.khalid1@gmail.com>
2023-12-28 18:59:32 +0500

    Working assistant before database integration

--- app.py
+++ app.py
@@ -1,36 +1,50 @@
+import json
 import streamlit as st
+import streamlit_lottie as st_lottie
 from model import MessageContent,ChatAssistant
-st.title("Chatify--your chatty friend")
+
+with st.container():
+    st.title("Poetify--your chatty friend")
+    st.write("Your poetic assistant!")
+
+# def load_animation(filepath:str):
+#     with open(filepath) as f:
+#         return json.load(f)
+
+# chatbot_1 = load_animation('Animation.json')
+
+# st_lottie(chatbot_1)
 
 instructions:str = """You are a poetic assistant, skilled in responding to 
                       the questions from all fields of life with a poetic flair"""
-# if "chat_assistant" not in st.session_state:
-#     st.session_state.chat_assistant = ChatAssistant()tate
+name : str = 'Poetic Assistant'
+if "chat_assistant" not in st.session_state:
+    st.session_state.chat_assistant = ChatAssistant(name,instructions)
 
 if "messages" not in st.session_state:
      st.session_state.messages = []
 
-for m in st.session_state.messages:
-    with st.chat_message(m["role"]):
-        st.markdown(m["content"])
+for m in st.session_state.chat_assistant.get_message_history():
+    with st.chat_message(m.role):
+        st.markdown(m.msgstring)
 
-# if "chat_assistant" in st.session_state: 
-#     for m in st.session_state.chat_assistant.get_message_history():
-#         with st.chat_message(m.role):
-#             st.markdown(m.content)
+
 
 if prompt := st.chat_input("Please Ask a Question"):
     with st.chat_message("user"):
          st.markdown(prompt)
-         st.session_state.chat_assistant = ChatAssistant(prompt,instructions)
-         st.session_state.messages.append({"role":"user","content":prompt})
-  
-        
+         st.session_state.chat_assistant.askquestion(prompt)
        
-    response  = st.session_state.chat_assistant.get_answer()
-    st.session_state.messages.append({"role":"assistant","content":response})
-    with st.chat_message("assistant"):
-        st.markdown(response)
+   
+       
+    if(st.session_state.chat_assistant.is_complete()):
+            response  = st.session_state.chat_assistant.get_answer()
+            with st.chat_message("assistant"):
+                 st.markdown(response)
+   
+   
+        
+
            
            
                 
--- model.py
+++ model.py
@@ -1,5 +1,9 @@
+import time
 from openai import OpenAI
-from openai.types.chat.chat_completion import ChatCompletion
+from openai.types.beta import Assistant
+from openai.types.beta.thread import Thread
+from openai.types.beta.threads.thread_message import ThreadMessage
+from openai.types.beta.threads.run import Run
 from dotenv import load_dotenv,find_dotenv
 from typing import Any
 
@@ -11,32 +15,76 @@ class MessageContent:
 
 
 class ChatAssistant:
-    def __init__(self,prompt:str,instructions:str,model="gpt-3.5-turbo-1106"):
-        self.prompt :str = prompt;
+    def __init__(self,name:str,instructions:str,model="gpt-3.5-turbo-1106"):
+        self.name :str = name;
         self.instructions:str = instructions;
         self.model:str = model;
 
         load_dotenv(find_dotenv());
         self.client : OpenAI = OpenAI();
-        self.chat_assistant:ChatCompletion = self.client.chat.completions.create(
+        self.chat_assistant:Assistant = self.client.beta.assistants.create(
+              name=self.name,
               model = self.model,
-              messages= [
-                {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming with creative flair."},
-                {"role": "user", "content": self.prompt}
-                    ],
-                    
-
-        )
+              instructions = self.instructions
+           )
+        """
+        #Thread is being created
+        """
+        self.thread: Thread  = self.client.beta.threads.create()   
         self.messages: list[MessageContent] = []
-        self.add_message(MessageContent(role="user",msgstring=prompt))
+       # self.add_message(MessageContent(role="user",msgstring=prompt))
+       
         """
-        #This function will get the answer to the current question
+        #This fucntion will receive question from the user
         """
+    def askquestion(self,question:str):
+              latestthread: Thread = self.client.beta.threads.messages.create(
+                    thread_id = self.thread.id,
+                    role = "user",
+                    content=question
+              )
+              """
+              #Initiating the run of the thread
+              """
+              self.latest_run:Run = self.client.beta.threads.runs.create(
+                       thread_id=self.thread.id,
+                       assistant_id=self.chat_assistant.id,
+                       instructions=self.instructions
+              )
+              self.add_message(MessageContent( role ="user",msgstring=question))
+        
+    """
+    #This function will check the status of the run
+    #When this fucntion returns true,it would mean that the run has been completed
+    """
+    def is_complete(self)->bool:
+        print("Current Status-----",self.latest_run.status)
+        while self.latest_run.status != "completed":
+            time.sleep(1)
+            self.latest_run:Run = self.client.beta.threads.runs.retrieve(
+                  thread_id = self.thread.id,
+                  run_id = self.latest_run.id
+
+            )
+            print("Latest Status: ", self.latest_run)
+        return True
+        
+        
+        
+        
+    """
+    #This function will get the answer to the current question
+    """
     def get_answer(self)->MessageContent:
-            answer = MessageContent(role="assistant",msgstring=self.chat_assistant.choices[0].message.content)
-            print("Assistant:",answer.role,"Answet",answer.msgstring)
-            self.add_message(MessageContent(role="assistant",msgstring=self.chat_assistant.choices[0].message.content))
-            return self.chat_assistant.choices[0].message.content
+            response = self.client.beta.threads.messages.list(
+            thread_id=self.thread.id
+        )
+            
+            #print("Assistant:",answer.role,"Answet",answer.msgstring)
+            print("Response: ", response.data[0])
+            answer = MessageContent(response.data[0].role, response.data[0].content[0].text.value)
+            self.add_message(answer)
+            return answer.msgstring
             """
             #This function creates the message history by appending the message to the list of messages
             """
